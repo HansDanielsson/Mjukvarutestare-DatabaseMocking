e9d114db92be654e62f39e8efa8f56ed199bcee6
Hans Danielsson <Hans.Ro.Danielsson@gmail.com>
2024-11-23 20:00:12 +0100

    script edit

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-const { createUser, getAllUsers } = require('./dbService');
+const { createUser, getAllUsers } = require('./dbService')
 const syncDatabase = require('./dbSync');
 
 (async () => {
   await syncDatabase() // För att synka databasen med vår modell
 
   // Skapa en ny användare
-  let user = await createUser('Test3', 'test3@example.com') // Anropa vår ServiceKlass för att lägga till ny användare
+  const user = await createUser('Test5', 'test5@example.com') // Anropa vår ServiceKlass för att lägga till ny användare
   console.log('Ny användare skapad:', user.toJSON())
 
   // Hämta alla användare
-  let users = await getAllUsers() // Anropa vår Serviceklass för att hämta alla användare
+  const users = await getAllUsers() // Anropa vår Serviceklass för att hämta alla användare
   console.log('Alla användare:', users.map((u) => u.toJSON()))
-
 })()
