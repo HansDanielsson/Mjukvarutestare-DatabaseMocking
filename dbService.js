40f8a81467f2b611e96a3d4628cd6d15762636eb
Hans Danielsson <Hans.Ro.Danielsson@gmail.com>
2024-11-23 17:00:37 +0100

    dbService changed

--- dbService.js
+++ dbService.js
@@ -1,11 +1,11 @@
 const User = require('./Models/user')
 
-async function createUser(name, email) {
-  return await User.create( {name, email})
+async function createUser (name, email) {
+  return await User.create({ name, email })
 }
 
-async function getAllUsers() {
+async function getAllUsers () {
   return await User.findAll()
 }
 
-module.exports = { createUser, getAllUsers}
+module.exports = { createUser, getAllUsers }
